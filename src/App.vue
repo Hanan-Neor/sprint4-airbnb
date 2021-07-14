192cbbe0eb21f56614e7ed38c48fed088091ce6d
idogross1 <idogross1@gmail.com>
2021-07-14 12:42:51 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      hello
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
