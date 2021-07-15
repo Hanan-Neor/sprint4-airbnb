0581ea17ef90c724feb59dd51b114bf54cc52def
idogross1 <idogross1@gmail.com>
2021-07-16 00:11:11 +0300

    maps

--- src/cmps/google-maps.vue
+++ src/cmps/google-maps.vue
@@ -1,27 +1,42 @@
 <template>
-  <GmapMap
-    :center="{ lat: 10, lng: 10 }"
-    :zoom="7"
-    map-type-id="terrain"
-    style="width: 500px; height: 300px"
-  >
-    <GmapMarker
-      :key="index"
-      v-for="(m, index) in markers"
-      :position="m.position"
-      :clickable="true"
-      :draggable="true"
-      @click="center = m.position"
-    />
-  </GmapMap>
+  <section v-if="center">
+    <!-- {{ center }} -->
+    <GmapMap
+      :center="center"
+      :zoom="7"
+      map-type-id="terrain"
+      style="width: 100%; height: 480px"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        @click="center = m.position"
+        :icon="{ url: require('../assets/img/icons/home.svg') }"
+      />
+    </GmapMap>
+  </section>
 </template>
 
 <script>
 export default {
+  props: { loc: Object },
+
   data() {
     return {
-      markers: [],
+      center: {},
+      markers: [{ lat: 0, lng: 0 }],
+      location: { lat: 0, lng: 0 },
     };
+  },
+
+  created() {
+    this.center = { lat: this.loc.lat, lng: this.loc.lng };
+    console.log(this.center);
+    this.markers.push({
+      position: this.center,
+    });
   },
 };
 </script>
